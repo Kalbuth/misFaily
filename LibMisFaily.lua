5c53c7a024f2936fab153654e7a13b64cde96e54
kalbuth <kalbuth@gmail.com>
2019-02-25 22:04:33 +0100

    Realigned scripts with server version

--- LibMisFaily.lua
+++ LibMisFaily.lua
@@ -201,6 +201,74 @@ function setContains(set, key)
 end
 
 do
+	ACT_ROUTE_TARGET = { 
+    ClassName = "ACT_ROUTE_TARGET",
+  }
+	
+	function ACT_ROUTE_TARGET:New( TargetZone )
+    local self = BASE:Inherit( self, ACT_ROUTE_ZONE:New( TargetZone ) )
+		
+		return self
+	end
+	
+	function ACT_ROUTE_TARGET:onenterReporting( ProcessUnit, From, Event, To )
+  
+    local ZoneVec2 = self.TargetZone:GetVec2()
+    local ZonePointVec2 = POINT_VEC2:New( ZoneVec2.x, ZoneVec2.y )
+    local TaskUnitVec2 = ProcessUnit:GetVec2()
+		local ZoneVec3 = self.TargetZone.ZoneGROUP:GetVec3()
+		local ZonePointVec3 = POINT_VEC3:New( ZoneVec3.x, ZoneVec3.y, ZoneVec3.z )
+    local TaskUnitPointVec2 = POINT_VEC2:New( TaskUnitVec2.x, TaskUnitVec2.y )
+    local RouteText = "Route to " .. TaskUnitPointVec2:GetBRText( ZonePointVec2 ) .. " km to target, altitude " .. ZonePointVec3:GetAltitudeText() .. " meters."
+		self:Message( RouteText )
+  end
+	
+	TASK_INTERCEPT = {
+		ClassName = "TASK_INTERCEPT",
+	}
+	
+	function TASK_INTERCEPT:New( Mission, SetGroup, TaskName, TargetSetUnit )
+		local self = BASE:Inherit( self, TASK:New( Mission, SetGroup, TaskName, "Interception" ) )
+		self:F()
+		
+		local tmpTgtGroup
+		local tmpTgtName
+		local tmpRandomIndex = math.random(100, 10000)
+		
+		self.TargetSetUnit = TargetSetUnit
+				
+		for unitID, unitData in pairs(TargetSetUnit:GetSet()) do
+			tmpTgtGroup = unitData:GetGroup()
+			tmpTgtName = unitData:GetCallsign()
+		end
+		
+--		local zoneName = "ZONE_INTERCEPT_" .. tmpTgtName .."_" .. tostring(tmpRandomIndex)
+		local zoneName = "ZONE_INTERCEPT_" .. tostring(tmpRandomIndex)
+		
+		self.TargetZone = ZONE_GROUP:New( zoneName, tmpTgtGroup, 1000 )
+		self.TargetZone:E( "Group Zone created for task :" .. routines.utils.oneLineSerialize(self.TargetZone) )
+		local Fsm = self:GetUnitProcess()
+		Fsm:AddProcess ( "Planned", "Accept", ACT_ASSIGN_ACCEPT:New( self.TaskBriefing ), { Assigned = "Route", Rejected = "Eject" } )
+		Fsm:AddProcess ( "Assigned", "Route", ACT_ROUTE_TARGET:New( self.TargetZone ), { Arrived = "Update" } )
+		Fsm:AddTransition( "Rejected", "Eject", "Planned" )
+		Fsm:AddTransition( "Arrived", "Update", "Updated" )
+		Fsm:AddProcess ( "Updated", "Account", ACT_ACCOUNT_DEADS:New( self.TargetSetUnit, "Interception" ), { Accounted = "Success" } )
+		Fsm:AddTransition( "Accounted", "Success", "Success" )
+		Fsm:AddTransition( "Failed", "Fail", "Failed" )
+		
+		function Fsm:onenterUpdated( TaskUnit )
+			self:E( { self } )
+			self:Account()
+		end
+		
+		return self
+	
+	end
+	
+	function TASK_INTERCEPT:GetPlannedMenuText()
+		return self:GetStateString() .. " - " .. self:GetTaskName() .. " ( " .. self.TargetSetUnit:GetUnitTypesText() .. " )"
+	end
+	
 	GROUND_ZONE_CC = {
     ClassName = "GROUND_ZONE_CC",
   }
@@ -238,6 +306,62 @@ do
 		end
 	end
   
+  CSAR_HANDLER = {
+    ClassName = "CSAR_HANDLER",
+  }
+	
+	function CSAR_HANDLER:New( HostileZone, HostileSpawnList )
+  	local self = BASE:Inherit( self, BASE:New() )
+    self:F( HostileZone )
+  
+    self.HostileZone = HostileZone
+    self.HostileSpawnList = HostileSpawnList
+    
+    self.MaxHostileSpawnRange = 10000
+    self.MinHostileSpawnRange = 7000
+    self.MaxHostileMoveRange = 4000
+    self.MinHostileMoveRange = 2000
+    self.ZoneID = 1
+    
+    self:HandleEvent( EVENTS.Birth )
+    
+    return self
+	end
+	
+	function CSAR_HANDLER:OnEventBirth( EventData )
+	 local RescueGroup = EventData.IniGroup
+	 local RescueUnit = RescueGroup:GetUnit( 1 )
+	 local RescueUnitName = EventData.IniUnitName
+	 if string.find(RescueUnitName, "Wounded Pilot", 1) then 
+	   if ( self.HostileZone:IsPointVec3InZone( RescueUnit:GetPointVec3() )) then
+	     local SpawnZone = ZONE_UNIT:New( "ZONE_SPAWN_" .. self.ClassName .. self.ClassID .. "_" .. tostring(self.ZoneID), RescueUnit, self.MaxHostileSpawnRange )
+	     local MoveZone = ZONE_UNIT:New( "ZONE_MOVE_" .. self.ClassName .. self.ClassID .. "_" .. tostring(self.ZoneID), RescueUnit, self.MaxHostileMoveRange )
+	     self.ZoneID = self.ZoneID + 1
+	     for SpawnID, SpawnData in pairs(self.HostileSpawnList) do
+	       local SpawnPoint = SpawnZone:GetRandomPointVec2(self.MinHostileSpawnRange)
+	       local MovePoint = MoveZone:GetRandomPointVec2(self.MinHostileMoveRange)
+	       local HostileGroup = SpawnData:SpawnFromVec2( SpawnPoint:GetVec2() )
+	       local TaskHostile = HostileGroup:TaskRouteToVec2( MovePoint:GetVec2(), 40 )
+	       self:E( "Task for ground group: " .. routines.utils.oneLineSerialize(TaskHostile))
+	       -- HostileGroup:SetTask( TaskHostile )
+	     end
+	   end
+	 end
+	end
+	
+	function CSAR_HANDLER:SetSpawnParams( MaxSpawnRange, MinSpawnRange )
+	 self:F2( MaxSpawnRange, MinSpawnRange )
+	 self.MaxHostileSpawnRange =MaxSpawnRange
+	 self.MinHostileSpawnRange =MinSpawnRange
+	 return self
+	end
+	
+	function CSAR_HANDLER:SetMoveParams( MaxMoveRange, MinMoveRange )
+	 self:F2( MaxMoveRange, MinMoveRange )
+   self.MaxHostileMoveRange = MaxMoveRange
+   self.MinHostileMoveRange = MinMoveRange
+   return self
+  end
   
 	WARZONE_HANDLER = {
 		ClassName = "WARZONE_HANDLER"
--- MissionConfig.lua
+++ MissionConfig.lua
@@ -8,46 +8,14 @@ local obj1 = {
 			["RedIndex"] = 1;
 		};
 		["Center"] = {
-			["RedIndex"] = 3;
+			["RedIndex"] = 1;
 		};
 		["NovoNorth"] = {
-			["RedIndex"] = 2;
+			["RedIndex"] = 0;
 		};
 	};
 	["UnpackedUnits"] = {
-		[1] = {
-			["y"] = 176.46574401855;
-			["x"] = -23881.72265625;
-			["Template"] = "TEMPLATE_NONA";
-			["z"] = 266102.71875;
-			["Generation"] = 6975;
-			["Player"] = "Kalbuth";
-		};
-		[2] = {
-			["y"] = 6.4811034202576;
-			["x"] = -41984.45703125;
-			["Template"] = "TEMPLATE_CHAPARRAL";
-			["z"] = 291348.84375;
-			["Generation"] = 6996;
-			["Player"] = "Kalbuth";
-		};
-		[3] = {
-			["y"] = 176.46574401855;
-			["x"] = -23881.72265625;
-			["Template"] = "TEMPLATE_NONA";
-			["z"] = 266102.71875;
-			["Generation"] = 7158;
-			["Player"] = "Kalbuth";
-		};
-		[4] = {
-			["y"] = 176.46574401855;
-			["x"] = -23881.72265625;
-			["Template"] = "TEMPLATE_NONA";
-			["z"] = 266102.71875;
-			["Generation"] = 7069;
-			["Player"] = "Kalbuth";
-		};
 	};
-	["Generation"] = 7407;
+	["Generation"] = 40709;
 }
 return obj1
